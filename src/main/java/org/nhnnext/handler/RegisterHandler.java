d5643c6aa8655e6cce5ece20128a97c0aaad26fb
reaperes <emerald105@hanmail.net>
2014-10-07 20:04:24 +0900

    first initial ios push skeleton app

--- src/main/java/org/nhnnext/handler/RegisterHandler.java
+++ src/main/java/org/nhnnext/handler/RegisterHandler.java
@@ -16,15 +16,37 @@ public class RegisterHandler extends BaseHandler {
     @Override
     public void handle(JSONObject jsonObject) {
         try {
-            HttpRequestWithBody httpRequestWithBody = Unirest.post(UNIQUSH_BASE_URL);
-            httpRequestWithBody.header("Content-Type", "application/x-www-form-urlencoded");
-            MultipartBody body = httpRequestWithBody.field("service", config.getString("uniqush.service"))
-                    .field("subscriber", jsonObject.getString("device_name"))
-                    .field("pushservicetype", jsonObject.getString("device_type"))
-                    .field("regid", jsonObject.getString("device_id"));
-            HttpResponse<JsonNode> response = body.asJson();
-            if (response.getCode() != 200)
-                System.out.println("Error");
+            String subscriber = jsonObject.getString("device_name");
+            String deviceType = jsonObject.getString("device_type");
+            String deviceId = jsonObject.getString("device_id");
+
+            if (deviceType.equals("android")) {
+                HttpRequestWithBody httpRequestWithBody = Unirest.post(UNIQUSH_BASE_URL);
+                httpRequestWithBody.header("Content-Type", "application/x-www-form-urlencoded");
+                MultipartBody body = httpRequestWithBody
+                        .field("service", config.getString("uniqush.service"))
+                        .field("subscriber", jsonObject.getString("device_name"))
+                        .field("pushservicetype", jsonObject.getString("device_type"))
+                        .field("regid", jsonObject.getString("device_id"));
+                HttpResponse<JsonNode> response = body.asJson();
+                if (response.getCode() != 200)
+                    System.out.println("Error");
+            }
+            else if (deviceType.equals("ios")) {
+                HttpRequestWithBody httpRequestWithBody = Unirest.post(UNIQUSH_BASE_URL);
+                httpRequestWithBody.header("Content-Type", "application/x-www-form-urlencoded");
+                MultipartBody body = httpRequestWithBody
+                        .field("service", config.getString("uniqush.service"))
+                        .field("subscriber", jsonObject.getString("device_name"))
+                        .field("pushservicetype", jsonObject.getString("device_type"))
+                        .field("devtoken", jsonObject.getString("device_id"));
+                HttpResponse<JsonNode> response = body.asJson();
+                if (response.getCode() != 200)
+                    System.out.println("Error");
+            }
+            else {
+                System.out.println("RegisterHandler Error: " + deviceType);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
